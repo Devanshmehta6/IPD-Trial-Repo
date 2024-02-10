0f390b8c1f780989fd693d727b1ec130e9eae925
devanshmehtaGitHub <devanshgithub@gmail.com>
2024-02-10 18:43:45 +0530

    allowed host changes

--- IPD/settings.py
+++ IPD/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-nis6hoofz_!a&r8x7z)c@x*pfn!b0mf0$6m!&o_#x-r=xa+9ls
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
